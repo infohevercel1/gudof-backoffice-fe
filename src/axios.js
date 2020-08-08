adc53d1540f1bdef8bcaa779b4e4402cfb5b41d0
utkarshsingh99 <utkarshsingh369@gmail.com>
2020-08-08 21:14:25 +0530

    URL change

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // axios.defaults.baseURL = 'https://infohebackoffice.herokuapp.com';
 export const instance = axios.create({
-    baseURL: process.env === 'PRODUCTION' ? 'https://infohebackoffice.herokuapp.com' : 'http://localhost:4000'
+    baseURL: 'https://infohebackoffice.herokuapp.com' 
+    // : 'http://localhost:4000'
 });
 
 // export default {instance};
