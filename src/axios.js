34115f33de2d3704d5d4da1d20900b59fb2db7bd
Nithin PS <47279496+Nithinps021@users.noreply.github.com>
2021-02-08 19:19:09 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios.defaults.baseURL = 'https://infohebackoffice.herokuapp.com';
 export const instance = axios.create({
-    baseURL: 'https://infohebackoffice.herokuapp.com' 
+    baseURL: 'https://infohebackend.herokuapp.com' 
     // baseURL : 'http://localhost:4000'
     // baseURL : 'https://infohebackend.herokuapp.com'
 
